79d3e9f782e0391993bb621b6209dec47d04f54b
Woogys <apolontes@hanmail.net>
2022-02-04 11:57:28 +0900

    Initial commit

---
+++ src/main/java/com/woogy/bookprac/BookpracApplication.java
@@ -0,0 +1,13 @@
+package com.woogy.bookprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woogy/bookprac/BookpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woogy.bookprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
