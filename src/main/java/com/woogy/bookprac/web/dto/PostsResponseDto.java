587b06e38209a5749074ca90df644ecfb1ca15ec
Woogys <apolontes@hanmail.net>
2022-02-12 15:02:56 +0900

    add - PostsResponseDto

---
+++ src/main/java/com/woogy/bookprac/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.woogy.bookprac.web.dto;
+
+import com.woogy.bookprac.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
