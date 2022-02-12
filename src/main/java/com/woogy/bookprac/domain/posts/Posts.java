ea9944d397c4a8efe84a736fea4991800ff0025b
Woogys <apolontes@hanmail.net>
2022-02-12 19:00:38 +0900

    Feat: Posts 클래스의 BaseTimeEntity 상속

--- src/main/java/com/woogy/bookprac/domain/posts/Posts.java
+++ src/main/java/com/woogy/bookprac/domain/posts/Posts.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
