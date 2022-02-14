f8cc00fc775a445502074e261d96893a124df4c1
Woogys <apolontes@hanmail.net>
2022-02-14 18:50:40 +0900

    Feat: 전체 조회를 위한 PostsListResponseDto 클래스 생성

---
+++ src/main/java/com/woogy/bookprac/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.woogy.bookprac.web.dto;
+
+import com.woogy.bookprac.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
