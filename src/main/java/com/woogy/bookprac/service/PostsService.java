f2c110045ed04bf31a146d7fec593e12910f4d17
Woogys <apolontes@hanmail.net>
2022-02-14 18:50:22 +0900

    Feat: 전체 조회를 위한 PostsService 코드 추가

--- src/main/java/com/woogy/bookprac/service/PostsService.java
+++ src/main/java/com/woogy/bookprac/service/PostsService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +37,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
