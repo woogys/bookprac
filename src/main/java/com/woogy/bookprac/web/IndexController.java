4ea4417339a0a2420e2545d83676a73bed0d58e5
Woogys <apolontes@hanmail.net>
2022-02-18 14:44:25 +0900

    Feat: index.mustache에서 userName을 사용할 수 있도록 model에 저장

--- src/main/java/com/woogy/bookprac/web/IndexController.java
+++ src/main/java/com/woogy/bookprac/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.woogy.bookprac.web;
 
+import com.woogy.bookprac.config.auth.dto.SessionUser;
+import javax.servlet.http.HttpSession;
 import com.woogy.bookprac.service.PostsService;
 import com.woogy.bookprac.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
