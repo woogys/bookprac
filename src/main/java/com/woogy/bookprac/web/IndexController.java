d59404491939d0eb5b6fb0ca64122479b2fbd1d2
Woogys <apolontes@hanmail.net>
2022-02-18 19:00:19 +0900

    Feat: 생성한 @LoginUser annotation으로 기존 코드 대체

--- src/main/java/com/woogy/bookprac/web/IndexController.java
+++ src/main/java/com/woogy/bookprac/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.woogy.bookprac.web;
 
+import com.woogy.bookprac.config.auth.LoginUser;
 import com.woogy.bookprac.config.auth.dto.SessionUser;
 import javax.servlet.http.HttpSession;
 import com.woogy.bookprac.service.PostsService;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null){
             model.addAttribute("userName", user.getName());
         }
